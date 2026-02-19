056422cd0bc0b798d41d016e4f5c0ab555dabfd8
Mark Waite <mark.earl.waite@gmail.com>
2026-02-19 11:40:34 -0700

    Add missing import for assertFalse

--- src/test/java/au/com/versent/jenkins/plugins/ignoreCommitterStrategy/IgnoreCommitterStrategySimpleTest.java
+++ src/test/java/au/com/versent/jenkins/plugins/ignoreCommitterStrategy/IgnoreCommitterStrategySimpleTest.java
@@ -25,6 +25,7 @@ package au.com.versent.jenkins.plugins.ignoreCommitterStrategy;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
